9fc67932711ee04a3e9f04b190938dbf8fdb0262
Viktor Dimkovic <viktor@betterbot.com>
2022-10-07 20:23:59 +0200

    FIX

--- resources/js/components/Invitation.vue
+++ resources/js/components/Invitation.vue
@@ -11,7 +11,7 @@
                     v-if="detailsVisible"
                 >
                     <div class="p-6 pt-10">
-                        <div class="px-6">
+                        <div class="">
                             <div class="">
                                 <div class="relative flex py-3 items-center">
                                     <div
@@ -31,17 +31,17 @@
                                 <p class="flex justify-center py-1">
                                     Прием: 18:00 - 18:30 часот
                                 </p>
-                                <p class="flex justify-center align-start py-1">
+                                <p
+                                    class="sm:flex justify-center align-start py-1"
+                                >
                                     Локација:
                                     <a
                                         target="_blank"
-                                        class="flex justify-center align-center inline-block px-6 py-2 bg-transparent text-blue-600 font-medium text-xs leading-tight rounded bg-fuchsia-200 focus:text-blue-700 focus:fuchsia-200 focus:outline-none focus:ring-0 active:bg-fuchsia-300 active:text-blue-800 transition duration-300 ease-in-out ml-2"
+                                        class="flex justify-center align-center inline-block px-6 py-2 bg-transparent text-blue-600 font-medium leading-tight rounded bg-fuchsia-200 focus:text-blue-700 focus:fuchsia-200 focus:outline-none focus:ring-0 active:bg-fuchsia-300 active:text-blue-800 transition duration-300 ease-in-out ml-2 text-base"
                                         href="https://www.google.com/maps/place/Centropalas+letna+gradina/@42.1647837,21.7508051,17z/data=!3m1!4b1!4m5!3m4!1s0x135451e75876c7ff:0xa498503cb01ca030!8m2!3d42.1647837!4d21.7529991"
                                         ><img
-                                            src="../../images/restoran.png"
+                                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Google_Maps_icon_%282020%29.svg/16px-Google_Maps_icon_%282020%29.svg.png"
                                             class="mr-2"
-                                            width="14"
-                                            height="14"
                                             alt="Локација"
                                         /><span class="pt-1"
                                             >Центропалас Летна Градина,
@@ -86,7 +86,7 @@
                         <Transition name="slide-fade">
                             <div class="">
                                 <div class="justify-center">
-                                    <div class="px-6 pt-4">
+                                    <div class="pt-4">
                                         <!-- <h5
                                             class="text-gray-900 text-xl font-medium mb-2 flex justify-center"
                                         >
@@ -99,7 +99,7 @@
                                                 class="flex-grow border-t border-gray-400"
                                             ></div>
                                             <span
-                                                class="flex-shrink mx-4 text-gray-900 text-3xl"
+                                                class="flex-shrink mx-4 text-gray-900 text-2xl"
                                                 >Потврди присуство</span
                                             >
                                             <div
@@ -109,7 +109,7 @@
                                         <div
                                             v-for="(user, index) in data"
                                             :key="index"
-                                            class="flex justify-start ml-0 sm:ml-4 md:ml-10 lg:ml-24 xl:ml-32"
+                                            class="flex justify-start ml-10 sm:ml-4 md:ml-10 lg:ml-24 xl:ml-32"
                                         >
                                             <label
                                                 class="inline-flex items-center mt-3 cursor-pointer"
