9ff27e48e55c2563180a77ee6dbc9debe173c3f1
Viktor Dimkovic <viktor@betterbot.com>
2022-10-09 21:38:37 +0200

    test

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,6 @@ width: 100%;
 display: flex;
 justify-content: center;
 align-items: center;">
-    <a href="/skrieni-rute/custom-export">ALOOO</a>
     <img width="800" height="800" style="margin-left: auto; margin-right: auto;" class="mt-12"
         src="https://media.tenor.com/olN6fEddMAUAAAAC/eyebrow-russian.gif" alt="">
 
